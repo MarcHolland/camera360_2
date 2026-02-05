715723a5d6597f409e7e00f8a916cba733b8df2c
Marc Holland <marc.holland@faro.com>
2026-02-05 17:27:01 +0100

    Initial Commit

---
+++ camera360_2/camera360_2App.swift
@@ -0,0 +1,17 @@
+//
+//  camera360_2App.swift
+//  camera360_2
+//
+//  Created by Marc Holland on 05.02.26.
+//
+
+import SwiftUI
+
+@main
+struct camera360_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
